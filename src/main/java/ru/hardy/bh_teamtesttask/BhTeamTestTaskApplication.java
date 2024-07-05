258c9ce9d5e1188fcfb2785b9e50f081f77f1f5f
Hardy31 <alex-vav@list.ru>
2024-07-05 21:28:36 +0300

    Spring Boot init modules client and server

---
+++ src/main/java/ru/hardy/bh_teamtesttask/BhTeamTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.hardy.bh_teamtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhTeamTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhTeamTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hardy/bh_teamtesttask/BhTeamTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hardy.bh_teamtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhTeamTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
